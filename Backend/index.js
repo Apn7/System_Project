0729a1ad95b14e18c3c254f8671cccc05aeb5bec
Forhad Islam Rony <forhadrony161@gmail.com>
2025-08-10 22:36:23 +0600

    allow all origin

--- Backend/index.js
+++ Backend/index.js
@@ -40,7 +40,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: true, // For development, you might want to allow all origins
+  origin: '*', // Allow all origins
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
